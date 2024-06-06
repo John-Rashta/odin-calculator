f321aa03e961a662aac91b1a67bdfec8096d147c
John-Rashta <john_rashta@outlook.com>
2024-06-06 19:37:30 +0100

    Commit operators wiring
    Should have commited earlier but operators functions are done, equal
    sign works as intended, added the decimal options aswell

--- calculator.js
+++ calculator.js
@@ -10,7 +10,12 @@ function subtract(a, b) {
 
 function divide(a, b) {
 
-    return a / b;
+    if (b === 0) {
+        alert("Do not divide by 0");
+        clear();
+    } else {
+        return a / b;
+    }
 }
 
 function multiply(a, b) {
@@ -27,46 +32,146 @@ function operate(first, second, operator) {
     switch(operator) {
 
         case "+":
-            add(first, second);
-            break;
+            return add(first, second);
 
         case "-":
-            subtract(first, second);
-            break;
+            return subtract(first, second);
 
         case "/":
-            divide(first, second);
-            break;
+            return divide(first, second);
 
         case "x":
-            multiply(first, second);
-            break;
+            return multiply(first, second);
     }
 }
+
+function clear() {
+
+    firstNumber = undefined;
+    operatorSymbol = undefined;
+    secondNumber = undefined;
+    newNumber = true;
+    replaceOperator = false;
+
+    const display = document.querySelector(".currentDisplay");
+    display.textContent = "";
+
+}
+
+
 
 
 let firstNumber;
 let operatorSymbol;
 let secondNumber;
 let newNumber = true;
+let replaceOperator = false;
 
 const numberButtons = document.querySelector(".numbers");
 
 numberButtons.addEventListener("click", (event) => {
 
     let target = event.target;
+    const display = document.querySelector(".currentDisplay");
 
     if (target.tagName === "BUTTON" && !target.classList.contains("decimal")) {
 
-        const display = document.querySelector(".currentDisplay");
 
         if (newNumber) {
 
             display.textContent = target.textContent;
             newNumber = false;
+            replaceOperator = false;
         } else {
 
             display.textContent += target.textContent;
         }
     }
+
+    if (target.classList.contains("decimal")) {
+
+        if (display.textContent.includes(".")) {
+
+            return;
+
+        } else if (display.textContent) {
+
+            display.textContent += target.textContent;
+        }
+
+
+    }
 });
+
+const operations = document.querySelector(".operators");
+
+operations.addEventListener("click", (event) => {
+
+    let target = event.target;
+    const display = document.querySelector(".currentDisplay");
+
+    if (target.tagName === "BUTTON" && !target.classList.contains("operate")) {
+
+        
+        if (replaceOperator) {
+
+            operatorSymbol = target.textContent;
+        } else if (!Number(firstNumber)) {
+
+            firstNumber = display.textContent;
+            operatorSymbol = target.textContent;
+            newNumber = true;
+            replaceOperator = true;
+        } else if (!Number(secondNumber)) {
+
+            let first = Number(firstNumber);
+            let second = Number(display.textContent);
+
+            let result = operate(first, second, operatorSymbol);
+
+            firstNumber = result;
+
+            display.textContent = result;
+
+            second = undefined;
+
+            operatorSymbol = target.textContent;
+
+            newNumber = true;
+            
+            replaceOperator = true;
+
+        }
+
+
+    }
+
+    if (target.classList.contains("operate")) {
+
+        if(!Number(firstNumber)) {
+
+            return;
+        } else if (newNumber) {
+
+            return;
+        } else {
+        
+            let first = Number(firstNumber);
+            let second = Number(display.textContent);
+
+            let result = operate(first, second, operatorSymbol);
+
+            firstNumber = undefined;
+            operatorSymbol = undefined;
+            secondNumber = undefined;
+
+            display.textContent = result;
+
+            newNumber = true;
+        }
+    }
+});
+
+let clearButton = document.querySelector(".clear");
+
+clearButton.addEventListener("click", clear);
